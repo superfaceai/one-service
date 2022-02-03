5c7dee8029224044b789e45d0e07e74af5f77a23
Jan Vlnas <616767+jnv@users.noreply.github.com>
2022-02-03 17:16:00 +0100

    chore(spec): Add spec for createGraphQLMiddleware (#13)
    Co-authored-by: Ondrej Musil <o@superface.ai>

---
+++ src/graphql.spec.ts
@@ -0,0 +1,46 @@
+import { createGraphQLMiddleware } from './graphql';
+import { createSchema } from './schema';
+import { mocked } from 'jest-mock';
+import { GraphQLSchema } from 'graphql';
+
+jest.mock('./schema');
+
+describe('graphql', () => {
+  beforeEach(() => {
+    mocked(createSchema).mockResolvedValue(new GraphQLSchema({}));
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('createGraphQLMiddleware', () => {
+    describe('with no options', () => {
+      it('returns a middleware with generated schema', async () => {
+        await expect(createGraphQLMiddleware()).resolves.toEqual(expect.any(Function));
+        expect(createSchema).toHaveBeenCalled();
+      });
+
+      describe('with createSchema error', () => {
+        beforeEach(() => {
+          mocked(createSchema).mockRejectedValueOnce(
+            new Error('Unable to generate, super.json not found'),
+          );
+        });
+
+        it('throws upon initialization', async () => {
+          await expect(createGraphQLMiddleware()).rejects.toThrowError();
+        });
+      });
+    });
+
+    describe('with invalid schema', () => {
+      it('throws an error', async () => {
+        // @ts-expect-error Intentional error
+        const middleware = createGraphQLMiddleware({ schema: true });
+        await expect(middleware).rejects.toThrowErrorMatchingInlineSnapshot(
+          `"Property \\"schema\\" is missing"`,
+        );
+      });
+    });
+  });
+});

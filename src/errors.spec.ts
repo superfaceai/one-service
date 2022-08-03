04c80b87e2bd3decf39e809ff66611ae79d65238
Jan Vlnas <jan.vlnas@superface.ai>
2022-08-03 13:10:10 +0200

    fix: Add tests

---
+++ src/errors.spec.ts
@@ -0,0 +1,73 @@
+import { isOneSdkError, remapOneSdkError } from './errors';
+
+describe('errors', () => {
+  describe('isOneSdkError', () => {
+    it('accepts any object with message property', () => {
+      expect(isOneSdkError({ message: 'foo' })).toBe(true);
+    });
+
+    it('accepts an Error object', () => {
+      expect(isOneSdkError(new Error('bar'))).toBe(true);
+    });
+
+    it("doesn't accept null", () => {
+      expect(isOneSdkError(null)).toBe(false);
+    });
+
+    it("doesn't accept anything else", () => {
+      expect(isOneSdkError(['baz'])).toBe(false);
+      expect(isOneSdkError('foobar')).toBe(false);
+    });
+  });
+
+  describe('remapOneSdkError', () => {
+    it('wraps object to Error', () => {
+      expect(
+        remapOneSdkError({ name: 'FooBar', message: 'Some message' }),
+      ).toBeInstanceOf(Error);
+    });
+
+    it('keeps originalError in a property', () => {
+      const input = { name: 'FooBar', message: 'Some message' };
+      const result = remapOneSdkError(input);
+      expect(result.message).toBe(input.message);
+      expect(result.originalError).toEqual(input);
+    });
+
+    it('copies over selected properties from the input', () => {
+      const extensions = {
+        kind: 'Kind',
+        properties: {
+          foo: true,
+          bar: ['baz'],
+        },
+        statusCode: '200',
+        response: {
+          body: 'foo',
+        },
+        metadata: {
+          foo: 'bar',
+        },
+      };
+
+      const result = remapOneSdkError({
+        message: 'Message',
+        name: 'Error',
+        ...extensions,
+      });
+      expect(result.extensions).toEqual(extensions);
+    });
+
+    it('ignores other properties from the input error', () => {
+      const input = {
+        message: 'Message',
+        name: 'Error',
+        foo: 'bar',
+      };
+
+      const result = remapOneSdkError(input);
+      expect(result).not.toHaveProperty('foo');
+      expect(result.extensions).not.toHaveProperty('foo');
+    });
+  });
+});

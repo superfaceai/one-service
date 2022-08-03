7ea617650772ba288f3106e81171b61dbbf67329
Jan Vlnas <jan.vlnas@superface.ai>
2022-08-03 15:04:33 +0200

    feat: Export OneSdkResolverError

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const packageJson = require('../package.json');
 export * from './server';
 export * from './graphql';
 export * from './schema';
+export { OneSdkResolverError } from './errors';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 export const VERSION: string = packageJson.version;

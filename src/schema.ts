3c5119c5c6fd0d54d56f8616ae9111c89fe40f8e
Ondřej Musil <o@superface.ai>
2023-03-06 17:59:36 +0100

    --wip-- [skip ci]

--- src/schema.ts
+++ src/schema.ts
@@ -9,19 +9,35 @@ import { anonymizeSuperJson } from '@superfaceai/one-sdk/dist/core/events/report
 import { NormalizedSuperJsonDocument } from '@superfaceai/ast';
 import createDebug from 'debug';
 import {
+  ArgumentNode,
   GraphQLFieldConfig,
   GraphQLFieldConfigMap,
   GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
+  Kind,
+  OperationDefinitionNode,
+  OperationTypeNode,
+  print,
   printSchema,
+  SelectionNode,
+  SelectionSetNode,
 } from 'graphql';
 import { join as joinPath } from 'path';
 import { DEBUG_PREFIX } from './constants';
 import { load as loadProfile } from './profile';
 import { generateProfileConfig, generateProfileTypes } from './schema.types';
-import { hasFieldsDefined, sanitizedProfileName } from './schema.utils';
+import {
+  hasFieldsDefined,
+  sanitizedProfileName,
+  typeFromSafety,
+} from './schema.utils';
+import {
+  getProfileOutput,
+  ObjectStructure,
+  StructureType,
+} from '@superfaceai/parser';
 
 const debug = createDebug(`${DEBUG_PREFIX}:schema`);
 
@@ -105,10 +121,10 @@ export async function generate(
     }),
     mutation: hasFieldsDefined(mutationFields)
       ? new GraphQLObjectType({
-          name: 'Mutation',
-          description: "Profile's unsafe and idempotent use-cases",
-          fields: mutationFields,
-        })
+        name: 'Mutation',
+        description: "Profile's unsafe and idempotent use-cases",
+        fields: mutationFields,
+      })
       : undefined,
   });
 
@@ -161,4 +177,129 @@ export function superJsonFieldConfig(): GraphQLFieldConfig<any, any> {
       };
     },
   };
+}
+
+function selectionSet(selections: SelectionNode[]): SelectionSetNode {
+  return {
+    kind: Kind.SELECTION_SET,
+    selections,
+  };
+}
+
+function fieldSelection(
+  name: string,
+  subFields?: SelectionNode[],
+  args?: ArgumentNode[],
+): SelectionNode {
+  return {
+    kind: Kind.FIELD,
+    name: {
+      kind: Kind.NAME,
+      value: name,
+    },
+    selectionSet: subFields ? selectionSet(subFields) : undefined,
+    arguments: args,
+  };
+}
+
+function isNested(structure: StructureType): boolean {
+  return (
+    structure.kind === 'ListStructure' ||
+    structure.kind === 'ObjectStructure' ||
+    structure.kind === 'NonNullStructure'
+  );
+}
+
+function resultFields(structure: StructureType): SelectionNode[] {
+  if (structure.kind === 'NonNullStructure') {
+    return resultFields(structure.value);
+  }
+
+  if (structure.kind === 'ObjectStructure') {
+    return Object.entries(structure.fields ?? {}).map(
+      ([fieldName, fieldStructure]) => {
+        return fieldSelection(
+          fieldName,
+          isNested(fieldStructure) ? resultFields(fieldStructure) : undefined,
+        );
+      },
+    );
+  }
+
+  if (structure.kind === 'ListStructure') {
+    return resultFields(structure.value);
+  }
+
+  return [];
+}
+
+function inputArgument(structure: ObjectStructure): ArgumentNode[] {
+  return [
+    {
+      kind: Kind.ARGUMENT,
+      name: {
+        kind: Kind.NAME,
+        value: 'input',
+      },
+      value: {
+        kind: Kind.OBJECT,
+        fields: Object.entries(structure.fields).map(([fieldName]) => {
+          return {
+            kind: Kind.OBJECT_FIELD,
+            name: {
+              kind: Kind.NAME,
+              value: fieldName,
+            },
+            value: {
+              kind: Kind.STRING,
+              value: '',
+            },
+          };
+        }),
+      },
+    },
+  ];
+}
+
+export async function generateOperationExample(
+  superJson: NormalizedSuperJsonDocument,
+  profileId: string,
+): Promise<string | undefined> {
+  const profile = await loadProfile(superJson, profileId);
+
+  const profileOutput = getProfileOutput(profile.ast);
+  const usecase = profileOutput.usecases[0];
+
+  if (!usecase) {
+    console.error('no usecase');
+    return;
+  }
+
+  const operation: OperationDefinitionNode = {
+    kind: Kind.OPERATION_DEFINITION,
+    operation:
+      typeFromSafety(usecase.safety) === 'query'
+        ? OperationTypeNode.QUERY
+        : OperationTypeNode.MUTATION,
+    name: {
+      kind: Kind.NAME,
+      value: 'Example',
+    },
+    selectionSet: selectionSet([
+      fieldSelection(sanitizedProfileName(profile.ast), [
+        fieldSelection(
+          usecase.useCaseName,
+          [
+            fieldSelection(
+              'result',
+              usecase.result ? resultFields(usecase.result) : undefined,
+            ),
+          ],
+          [...(usecase.input ? inputArgument(usecase.input) : [])],
+        ),
+      ]),
+    ]),
+  };
+
+  return print(operation);
 }

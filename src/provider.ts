4907894a34461f0f2f19071ad1a1bec5f2c21710
Ondřej Musil <o@superface.ai>
2023-03-27 15:17:09 +0200

    fix: add loading from registry

--- src/provider.ts
+++ src/provider.ts
@@ -1,7 +1,11 @@
 import { NormalizedSuperJsonDocument, ProviderJson } from '@superfaceai/ast';
 import {
   Config,
+  fetchProviderInfo,
+  NodeCrypto,
+  NodeFetch,
   NodeFileSystem,
+  NodeTimers,
   resolveProviderJson,
 } from '@superfaceai/one-sdk';
 
@@ -9,15 +13,33 @@ export async function load(
   superJson: NormalizedSuperJsonDocument,
   providerName: string,
 ): Promise<ProviderJson> {
-  const providerJson = await resolveProviderJson({
+  const config = new Config(NodeFileSystem);
+
+  // Load from local file
+  let providerJson = await resolveProviderJson({
     providerName,
     superJson,
     fileSystem: NodeFileSystem,
-    config: new Config(NodeFileSystem),
+    config,
   });
 
+  // Load from registry
   if (!providerJson) {
-    throw new Error(`Provider ${providerName} couldn't be resolved`); // TODO: custom error
+    try {
+      providerJson = await fetchProviderInfo(
+        providerName,
+        config,
+        new NodeCrypto(),
+        new NodeFetch(new NodeTimers()),
+      );
+    } catch (_e) {
+      // Ignore error
+    }
+  }
+
+  // Throw error if provider not found
+  if (!providerJson) {
+    throw new Error(`Provider '${providerName}' not found`);
   }
 
   return providerJson;

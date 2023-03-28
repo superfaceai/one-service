5373816f7775bb6393836c9e369ec233ad4dc65f
Ondřej Musil <o@superface.ai>
2023-03-28 17:50:39 +0200

    test: add more tests for entire schema generation

--- src/schema.spec.ts
+++ src/schema.spec.ts
@@ -11,13 +11,34 @@ describe('schema', () => {
       const schema = await generate(
         await createSuperJson('profile_without_scope'),
       );
-
       expectSchemaValidationErrors(schema);
       expectSchema(schema);
     });
 
     it('generates valid schema for usecases mapped to mutation only', async () => {
       const schema = await generate(await createSuperJson('unsafe_only'));
+      expectSchemaValidationErrors(schema);
+      expectSchema(schema);
+    });
+
+    it('generates valid schema for profile with mutiple usecases', async () => {
+      const schema = await generate(
+        await createSuperJson('profile_multiple_use_cases'),
+      );
+      expectSchemaValidationErrors(schema);
+      expectSchema(schema);
+    });
+
+    it('generates valid schema for usecase with empty input', async () => {
+      const schema = await generate(
+        await createSuperJson('profile_with_empty_input_structure'),
+      );
+      expectSchemaValidationErrors(schema);
+      expectSchema(schema);
+    });
+
+    it('generates valid schema for usecase without result', async () => {
+      const schema = await generate(await createSuperJson('no_result'));
       expectSchemaValidationErrors(schema);
       expectSchema(schema);
     });
